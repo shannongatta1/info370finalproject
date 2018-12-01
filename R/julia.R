b925d7249f585294cb4156fde79105a32f7c10f2
juliazartn <julia.zartn@gmail.com>
2018-12-01 10:32:23 -0800

    Added EDA for race w/pie charts

---
+++ R/julia.R
@@ -0,0 +1,38 @@
+library(ggplot2)
+library(dplyr)
+library(scales)
+
+data <- read.csv('../data/dem_candidates_prepped.csv', stringsAsFactors = FALSE)
+
+#Race
+white_can <- data %>% filter(Race == 'White')
+non_white_can <- data %>% filter(Race == 'Nonwhite')
+
+race <- data.frame(race=c("White", "Nonwhite"),
+                 value=c(nrow(white_can), nrow(non_white_can)))
+
+ggplot(data=race, aes(x=race, y=value, fill=race)) +
+  geom_bar(stat="identity")
+
+#pie chart
+piechart<- ggplot(race, aes(x="", y=value, fill=race))+
+  geom_bar(width = 1, stat = "identity") +
+  coord_polar("y", start=0) + 
+        geom_text(aes(y = value/2 + 
+        c(0, cumsum(value)[-length(value)]), 
+            label = percent(value/(nrow(white_can)+nrow(non_white_can)))), size=5)
+
+#success vs race
+white_can_success <- data %>% filter(Race == 'White') %>% filter(Primary.Status == "Advanced")
+non_white_can_success <- data %>% filter(Race == 'Nonwhite') %>% filter(Primary.Status == "Advanced")
+
+race_success <- data.frame(race=c("White", "Nonwhite"),
+                   value=c(nrow(white_can_success), nrow(non_white_can_success)))
+
+success_pie<- ggplot(race_success, aes(x="", y=value, fill=race))+
+  geom_bar(width = 1, stat = "identity") +
+  coord_polar("y", start=0) + 
+  geom_text(aes(y = value/2 + 
+                  c(0, cumsum(value)[-length(value)]), 
+                label = percent(value/(nrow(white_can_success)+nrow(non_white_can_success)))), size=5)
+
