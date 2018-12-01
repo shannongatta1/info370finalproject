d81bb216895dbb7dafd272384ed5b47aec32d450
Andrea Jorge <andrea_jorge814@msn.com>
2018-12-01 13:28:13 -0800

    fixed modeling data with dummies

--- andrea_EDA.R
+++ andrea_EDA.R
@@ -2,7 +2,7 @@ library(ggplot2)
 library(dplyr)
 
 dems <- read.csv('./data/dem_candidates_prepped.csv', stringsAsFactors = FALSE)
-
+dummies <- read.csv('./data/dem_with_dummies.csv', stringsAsFactors = FALSE)
 # bar chart:
 # x-axis - election month
 # y-axis - count
@@ -11,6 +11,8 @@ dems <- read.csv('./data/dem_candidates_prepped.csv', stringsAsFactors = FALSE)
 # map:
 # fill each state with a different color depending on the month of the election
 
+
+
 # pie chart for endorsements: --> facet grid
 # number of yes, no, neutral
 
@@ -18,16 +20,24 @@ dems <- read.csv('./data/dem_candidates_prepped.csv', stringsAsFactors = FALSE)
 # number of yes, no, neutral
 # color by primary status
 
-# [row, ]
-test <- dems[1,] %>% select(c(16:27)) 
-test$Neutral <- rowSums(test == 'Netural')
+count_endorsements <- dems %>% select(c(16:27))
+dems$Neutral.Endorsements <- rowSums(count_endorsements == 'Neutral')
+dems$Yes.Endorsements <- rowSums(count_endorsements == 'Yes')
+dems$No.Endorsements <- rowSums(count_endorsements == 'No')
 
-count_endorsements <- function(row) {
-  row <- row %>% select(c(16:27))
-  row
-}
+write.csv(dems, file = './data/dems_with_endorsement_counts.csv', row.names = FALSE)
 
-test2 <- dems %>% select(c(16:27))
-test2$Neutral <- rowSums(test2 == 'Netural')
-test2$Yes <- rowSums(test2 == 'Yes')
-test2$No <- rowSums(test2 == 'No')
+yes_endorsements <- dems %>% filter(Yes > 0) %>% group_by(Primary.Status) %>% count(Primary.Status)
+no_endorsements <- dems %>% filter(No > 0) %>% group_by(Primary.Status) %>% count(Primary.Status)
+neutral_endorsements <- dems %>% filter(Yes > 0, No > 0) %>% 
+                          group_by(Primary.Status) %>% 
+                          count(Primary.Status)
+
+yes_bar <- ggplot(yes_endorsements, aes(x=Primary.Status, y=n, fill=Primary.Status))+
+  geom_bar(stat = "identity")
+
+no_bar <- ggplot(no_endorsements, aes(x=Primary.Status, y=n, fill=Primary.Status))+
+  geom_bar(stat = "identity")
+
+neutral_bar <- ggplot(neutral_endorsements, aes(x=Primary.Status, y=n, fill=Primary.Status))+
+  geom_bar(stat = "identity")
